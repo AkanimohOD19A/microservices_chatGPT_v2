3b59cbd9c1969920e02259dbe066e9932a0eb58d
AfroLogicInsect <danielamahtoday@gmail.com>
2023-04-13 04:31:48 +0100

    introduced tone_int

--- app.py
+++ app.py
@@ -22,14 +22,18 @@ if cache_clear_dt("dummy") < date.today():
     cache.clear_cache()
 
 
-def create_action(adj_response_type, response_type):
-    # for adjective in adj_response_type:
-    #     prefix_adj = adjective.join(adjective).split(',')
-    strList = [str(i) for i in adj_response_type]
-    myString = ", ".join(strList)
-    prefix_keyword = myString + " and " + str(response_type)
-    return prefix_keyword
+# def create_action(adj_response_type, response_type):
+#     # for adjective in adj_response_type:
+#     #     prefix_adj = adjective.join(adjective).split(',')
+#     strList = [str(i) for i in adj_response_type]
+#     myString = ", ".join(strList)
+#     prefix_keyword = myString + " and " + str(response_type)
+#     return prefix_keyword
 
+def create_action(adj_response_type, response_type, revision_type):
+    strList = [str(adj_response_type), str(response_type), str(revision_type)]
+    prefix_keyword = ", ".join(strList)
+    return prefix_keyword
 
 def ChatGPT(user_query):
     '''
@@ -98,14 +102,19 @@ option = st.sidebar.selectbox("How would you like to run your query?", ("Use Key
 start = time.time()
 if option == "Use Keywords":
     ## Read a list of adjectives and actions
-    adjs = pd.read_csv('./prefix_data/adjectives.csv')
-    actions = pd.read_csv('./prefix_data/document.csv')
+    tone = pd.read_csv('./prefix_data/tone.csv')
+    comprehension = pd.read_csv('./prefix_data/comprehension.csv')
+    revisions = pd.read_csv('./prefix_data/revision.csv')
 
-    adj_response_type = st.sidebar.multiselect("Please specify keywords", adjs)
-    response_type = st.sidebar.selectbox("Please specify action", actions)
-    if adj_response_type != "" and response_type != "":
-        prefix_keyword = create_action(adj_response_type, response_type)
-        prefix_query = f'A {prefix_keyword} response to {user_query}'
+    tone_response_type = st.sidebar.selectbox("Please specify Tone*", tone)
+    comprehension_response_type = st.sidebar.selectbox("Please specify Reader Comprehension*", comprehension)
+    revision_response_type = st.sidebar.selectbox("Please specify Degree of Revision", revisions)
+    response_length = st.sidebar.slider("Please specify the length of words to return", 25, 750, 100)
+    if tone_response_type != "" and comprehension_response_type != "" \
+            or revision_response_type != "" or response_length != "":
+        prefix_keyword = create_action(tone_response_type, comprehension_response_type, revision_response_type)
+        prefix_query = f'Using Keywords: {prefix_keyword} provide a response to {user_query} ' \
+                       f'with a length of {response_length} words'
         st.sidebar.markdown('##')  ##-> Empty Space Divider
         if st.sidebar.button("Run"):
             with st.spinner('Your query is running...'):
@@ -147,17 +156,17 @@ with col3:
 st.sidebar.markdown("---")  ## Divider
 st.sidebar.info("We have a bunch of keywords to articulate our query, this might not meet your requirement. "
                 "Click on the button below to modify.")
-if st.sidebar.button("Edit Keywords"):
-    ### Lifecycle of Dataframe
-    ## Read csv
-    df1 = pd.read_csv('./prefix_data/actions.csv', names=['ACTIONS'], header=1)
-    df2 = pd.read_csv('./prefix_data/adjectives.csv', names=['ADJECTIVES'], header=1)
+# if st.sidebar.button("Edit Keywords"):
+#     ### Lifecycle of Dataframe
+#     ## Read csv
+#     df1 = pd.read_csv('./prefix_data/tone.csv', names=['ACTIONS'], header=1)
+#     df2 = pd.read_csv('./prefix_data/tone.csv', names=['ADJECTIVES'], header=1)
 
-    ## Editable Dataframe
-    editable_df = st.sidebar.experimental_data_editor(pd.concat([df1, df2], axis=1), num_rows="dynamic")
+#     ## Editable Dataframe
+#     editable_df = st.sidebar.experimental_data_editor(pd.concat([df1, df2], axis=1), num_rows="dynamic")
 
-    ## Update csv
-    editable_df['ACTIONS'].to_csv('./prefix_data/actions.csv', index=False)
-    editable_df['ADJECTIVES'].to_csv('./prefix_data/adjectives.csv', index=False)
+#     ## Update csv
+#     editable_df['ACTIONS'] = editable_df['ACTIONS'].to_csv('./prefix_data/tone.csv', index=False)
+#     editable_df['ADJECTIVES'] = editable_df['ADJECTIVES'].to_csv('./prefix_data/adjectives.csv', index=False)
 
 st.sidebar.info("Made with ❤ by the AfroLogicInsect")
